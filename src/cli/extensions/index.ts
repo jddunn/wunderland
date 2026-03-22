c796e421bfe7853bbe23d03269798cf648167c93
jddunn <johnnyfived@protonmail.com>
2026-03-22 05:39:58 -0700

    feat(cli): export installer, secret-prompter, recommender from extensions barrel

---
+++ src/cli/extensions/index.ts
@@ -0,0 +1,7 @@
+export { normalizeExtensionName, normalizeExtensionList } from './aliases.js';
+export { mergeExtensionOverrides } from './settings.js';
+export { detectPackageManager, installExtension, uninstallExtension } from './installer.js';
+export { promptForMissingSecrets } from './secret-prompter.js';
+export { getRecommendations, formatRecommendations } from './recommender.js';
+export type { ExtensionRecommendation } from './recommender.js';
+export type { SecretPromptResult } from './secret-prompter.js';
