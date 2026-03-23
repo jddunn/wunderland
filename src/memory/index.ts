8f2ed66c5c66233a269a76430526162d1852f462
jddunn <johnnyfived@protonmail.com>
2026-03-22 20:20:04 -0700

    feat(memory): add MemorySystemInitializer and barrel exports

---
+++ src/memory/index.ts
@@ -0,0 +1,3 @@
+export { createMemorySystem } from './MemorySystemInitializer.js';
+export type { MemorySystemConfig, MemorySystem, MemoryTurnResult } from './MemorySystemInitializer.js';
+export { injectMemoryContext, removeMemoryContext } from './TurnMemoryRetriever.js';
